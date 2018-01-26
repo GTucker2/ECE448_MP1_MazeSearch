5b5029a96da190fd788952ce8d73d5b8b976c5a0
GTucker2 <gtucker2@illinois.edu>
2018-01-25 21:08:13 -0600

    Base Files for Search
    Added some basic data structures and setup function definitions.

---
+++ search.py
@@ -0,0 +1,58 @@
+import stack
+import queue
+
+''' search.py
+    Griffin A. Tucker
+    FINAL DATE
+    This module allows the user to perform a set 
+        of searches on a given graph. The
+        implemented searches include the following:
+        bredth-first search, depth-first search,
+        greedy-first search, and A* search. 
+'''
+
+def bredth_first():
+    ''' bredth_first
+        Griffin A. Tucker
+        FINAL DATE
+        This function performs a bredth-first search on
+            a given graph.
+        This function returns 0 for now.
+    '''
+    # Declare a queue 
+    q = queue.Queue()
+    # Return result 
+    return 0
+
+def depth_first():
+    ''' depth_first
+        Griffin A. Tucker
+        FINAL DATE
+        This function performs a depth-first search on
+            a given graph.
+        This function returns 0 for now.
+    '''
+    # Declare a stack
+    s = stack.Stack()
+    # Return result 
+    return 0
+
+def greedy_first():
+    ''' greedy_first
+        Griffin A. Tucker
+        FINAL DATE
+        This function performs a greedy-first search on
+            a given graph.
+        This function returns 0 for now.
+    '''
+    return 0
+
+def a_star():
+    ''' a_star
+        Griffin A. Tucker
+        FINAL DATE
+        This function performs an A* search on a given
+             graph.
+        This function returns 0 for now.
+    '''
+    return 0
---
+++ stack.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.insert(0,item)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[0]
