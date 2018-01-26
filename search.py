3e6f995a9f213b38a796d31d7bbe068d511b22ea
GTucker2 <gtucker2@illinois.edu>
2018-01-25 23:47:11 -0600

    Added general BFS

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ import queue
         greedy-first search, and A* search. 
 '''
 
-def bredth_first():
+def bredth_first(tree, start, goal):
     ''' bredth_first
         Griffin A. Tucker
         FINAL DATE
@@ -19,9 +19,27 @@ def bredth_first():
             a given graph.
         This function returns 0 for now.
     '''
-    # Declare a queue 
+    # Check if the tree is valid
+    if tree is None: return 0
+
+    # Declare a queue and enqueue the start tree 
     q = queue.Queue()
-    # Return result 
+    q.enqueue(start) 
+
+    # While the q is not empty 
+    while q.size() > 0:
+        # Get the current tree from the queue and check for success
+        # If successful, return true. Otherwise, expand.
+        cur = queue.dequeue()
+        if cur is goal: 
+            return 1
+        else:
+            if cur.up is not None: q.enqueue(cur.up)
+            if cur.down is not None: q.enqueue(cur.down)
+            if cur.left is not None: q.enqueue(cur.right)
+            if cur.right is not None: q.enqueue(cur.left)
+    
+    # Return failure if goal was not found
     return 0
 
 def depth_first():
--- stack.py
+++ stack.py
@@ -1,15 +1,21 @@
+''' stack.py
+    Python stack tutorial © Copyright 2014 Brad Miller, David Ranum
+'''
 class Stack:
-    def __init__(self):
-        self.items = []
+     def __init__(self):
+         self.items = []
 
-    def isEmpty(self):
-        return self.items == []
+     def isEmpty(self):
+         return self.items == []
 
-    def push(self, item):
-        self.items.insert(0,item)
+     def push(self, item):
+         self.items.append(item)
 
-    def pop(self):
-        return self.items.pop(0)
+     def pop(self):
+         return self.items.pop()
 
-    def peek(self):
-        return self.items[0]
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
