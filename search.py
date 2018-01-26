c0d56e505cb5d34bacd778d568ab7a89d0547d80
GTucker2 <gtucker2@illinois.edu>
2018-01-25 23:56:09 -0600

    Cleaned up commenting style

--- search.py
+++ search.py
@@ -14,32 +14,35 @@ import queue
 def bredth_first(tree, start, goal):
     ''' bredth_first
         Griffin A. Tucker
-        FINAL DATE
+        January 25 2018
         This function performs a bredth-first search on
             a given graph.
-        This function returns 0 for now.
+        This function accepts a tree to perform the search
+            on, a start node to begin the search at, and
+            a goal node to search for. 
+        This function returns 1 if the goal is found and
+            0 if the goal is not found.
     '''
-    # Check if the tree is valid
-    if tree is None: return 0
+    # Check if the tree is valid. If it is not, return 0.
+    if tree is None: return 0 
 
-    # Declare a queue and enqueue the start tree 
-    q = queue.Queue()
-    q.enqueue(start) 
+    # Declare a queue and enqueue the starting node.
+    q = queue.Queue()         
+    q.enqueue(start)          
 
-    # While the q is not empty 
-    while q.size() > 0:
-        # Get the current tree from the queue and check for success
-        # If successful, return true. Otherwise, expand.
-        cur = queue.dequeue()
-        if cur is goal: 
-            return 1
-        else:
+    # While the q is not empty, get the next node on the queue
+    # and check for the goal. If at the goal, return 1. Else,
+    # expand the node to the queue. 
+    while q.size() > 0:       
+        cur = queue.dequeue() 
+        if cur is goal: return 1
+        else:                 
             if cur.up is not None: q.enqueue(cur.up)
             if cur.down is not None: q.enqueue(cur.down)
             if cur.left is not None: q.enqueue(cur.right)
             if cur.right is not None: q.enqueue(cur.left)
     
-    # Return failure if goal was not found
+    # Return 0 if goal was not found
     return 0
 
 def depth_first():
