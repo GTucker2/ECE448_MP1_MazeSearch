0645b8784c3e207e973f5f49f58541ba7fc226b8
Michael Racine <racine2@illinois.edu>
2018-01-28 20:06:21 -0600

    boi
    less line = good code

--- MazeTree.py
+++ MazeTree.py
@@ -1,52 +1,47 @@
-
+## author : Michael Racine 
+## date: 1/28/18
 
 
 class MazeTree:
 
-    #create node subclass
-    class MazeNode:
-        #initilize the nodes
-        def __init__(self):
-            self.data = None
-            self.x = None
-            self.y = None
-            self.up = None
-            self.down = None
-            self.left = None
-            self.right = None
-
-        #create a node given data and coordinates
-        def create_node(data, x, y):
-            node = MazeTree.MazeNode()
-            node.data = data
-            node.x = x
-            node.y = y
-            return node
-
-    #create default tree
+    #initilize the nodes
     def __init__(self):
-        self.root = MazeTree.MazeNode()
+        self.data = None
+        self.x = None
+        self.y = None
+        self.up = None
+        self.down = None
+        self.left = None
+        self.right = None
+        self.visited = False
+
+    #create a node given data and coordinates
+    def create_node(data, x, y):
+       node = MazeTree()
+       node.data = data
+       node.x = x
+       node.y = y
+       return node
 
     #create the tree based on data
     def create_tree(self, maze, start_x, start_y):
         #base cases. check if x,y are in the bounds of the maze and if the maze exists
         if maze is None:
             return None
-        if 0 > start_x:
-            if start_x > len(maze):
-                return None
-        if 0 > start_y:
-            if start_x > len(maze[0]):
-                return None
-        ret_tree = MazeTree()
-        ret_tree.root = ret_tree.MazeNode.create_node(maze[start_x][start_y], start_x, start_y)
+        if start_x < 0:
+            return None
+        if start_x > len(maze):
+            return None
+        if start_y < 0:
+            return None
+        if start_y > len(maze[0]):
+            return None
+
+        ret_tree = create_node(maze[start_x][start_y], start_x, start_y)
         #recurse to create the children
-        up = start_y + 1
-        down = start_y - 1
-        left = start_x - 1
-        right = start_x + 1
-        ret_tree.root.up = ret_tree.create_tree(maze, start_x, up)
-        ret_tree.root.down = ret_tree.create_tree(maze, start_x, down)
-        ret_tree.root.left = ret_tree.create_tree(maze, left, start_y)
-        ret_tree.root.right = ret_tree.create_tree(maze, right, start_y)
-        return ret_tree.root
+        ret_tree.up = create_tree(maze, start_x, start_y+1)
+        ret_tree.down = create_tree(maze, start_x, start_y-1)
+        ret_tree.left = create_tree(maze, start_x-1, start_y)
+        ret_tree.right = create_tree(maze, start_x+1, start_y)
+        #return the root of the tree
+        return ret_tree
