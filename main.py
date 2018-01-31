9a588ef75a90584c0e6f534e8f67440426a958e4
GTucker2 <gtucker2@illinois.edu>
2018-01-31 16:40:42 -0600

    ran some search tests
    need to fix tree structure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import createmaze
 import MazeTree
+import search
 
 # Get the maze to traverse
 filename = input("Please enter the name of the mazefile (.txt): ")              
@@ -8,11 +9,14 @@ filename = input("Please enter the name of the mazefile (.txt): ")
 ascii_maze = []                                                                      
 mazeinfo = createmaze.loadMaze(filename, ascii_maze)      
 
-outputname = input(" Please enter the desired outputfile name (.txt) ")
-createmaze.print_maze(ascii_maze, outputname)
-
 # Create a graph based on the maze data
 maze_tree = MazeTree.MazeTree()                                                
 maze_tree = maze_tree.create_tree(ascii_maze, mazeinfo.startpx, mazeinfo.startpy)
 
-# Perform DFS 
+maze_tree.visited = True
+print(maze_tree.right.data)#.right.down.left.visited)
+
+# Perform BFS and export solution
+#search.bredth_first(maze_tree, '.')
+#outputname = input(" Please enter the desired outputfile name (.txt) ")
+#createmaze.print_maze(ascii_maze, outputname)
--- search.py
+++ search.py
@@ -11,13 +11,13 @@ import queue
         greedy-first search, and A* search. 
 '''
 
-def bredth_first(tree, start, goal):
+def bredth_first(root, goal):
     ''' bredth_first
         Griffin A. Tucker
         January 25 2018
         Performs a bredth-first search on a given tree.
         Accepts:
-            tree  : a tree to perform the search on
+            root  : a tree to perform the search on
             start : a start node to begin from
             goal  : a goal node to search for
         Returns:
@@ -25,23 +25,27 @@ def bredth_first(tree, start, goal):
             0 : if the goal is not found
     '''
     # Check if the tree is valid. If it is not, return 0.
-    if tree is None: return 0 
+    if root is None: return 0 
+    print(root.data)
 
     # Declare a queue and enqueue the starting node.
     q = queue.Queue()         
-    q.enqueue(start)          
+    q.enqueue(root)    
 
     # While the q is not empty, get the next node on the queue
     # and check for the goal. If at the goal, return 1. Else,
     # expand the node to the queue. 
     while q.size() > 0:       
-        cur = queue.dequeue() 
-        if cur is goal: return 1
-        else:                 
-            if cur.up is not None: q.enqueue(cur.up)
-            if cur.down is not None: q.enqueue(cur.down)
-            if cur.left is not None: q.enqueue(cur.right)
-            if cur.right is not None: q.enqueue(cur.left)
+        cur = q.dequeue() 
+        print(cur.data)
+        if cur.visited is false:
+            if cur.data is goal: return 1
+            else:                 
+                if cur.up is not None: q.enqueue(cur.up)
+                if cur.down is not None: q.enqueue(cur.down)
+                if cur.left is not None: q.enqueue(cur.right)
+                if cur.right is not None: q.enqueue(cur.left)
+            cur.visited = true
     
     # Return 0 if goal was not found
     return 0
