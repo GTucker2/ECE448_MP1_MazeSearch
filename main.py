deece7fd49de976e747958f0ac6ac2c49396269c
GTucker2 <gtucker2@illinois.edu>
2018-02-03 18:37:08 -0600

    DFS works
    DFS is working for one goal point

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ filename = input("Please enter the name of the mazefile (.txt): ")
 searchtype = "none"
 searchtypes = ["BFS","DFS","GREEDY","ASTAR"]
 while searchtype not in searchtypes:
-    searchtype = input("Please enter the type of search you would like to perform: (BFS, DFS, GREEDY, ASTAR")  
-    if searchtype not in searchtypes: print("Searchtype not found. Please enter BFS, DFS, GREEDY, or ASTAR: ")
+    searchtype = input("Please enter the type of search you would like to perform: (BFS, DFS, GREEDY, ASTAR) ")  
+    if searchtype not in searchtypes: print("Searchtype not found. Please enter BFS, DFS, GREEDY, or ASTAR) ")
 
 # Declare an array for the maze ascii data and then load significant info into an obj
 ascii_maze = []                                                                      
@@ -21,10 +21,10 @@ maze_tree = MazeTree.MazeTree()
 maze_tree = maze_tree.create_tree(ascii_maze, mazeinfo, mazeinfo.startpx, mazeinfo.startpy)
 
 # Perform a search on the maze and export solution
-if searchtype is "BFS": search.bredth_first(ascii_maze, maze_tree, '.')
-elif searchtype is "DFS": search.depth_first(ascii_maze, maze_tree, '.')
-elif searchtype is "GREEDY": search.greedy_first()
-elif searchtype is "ASTAR": search.a_star()
-    a = 1
+if searchtype == "BFS": search.bredth_first(ascii_maze, maze_tree, '.')
+elif searchtype == "DFS": search.depth_first(ascii_maze, maze_tree, '.')
+elif searchtype == "GREEDY": search.greedy_first()
+elif searchtype == "ASTAR": search.a_star()
+
 outputname = input(" Please enter the desired outputfile name (.txt) ")
 createmaze.print_maze(ascii_maze, outputname)
--- search.py
+++ search.py
@@ -26,9 +26,7 @@ def bredth_first(maze_data, root, goal):
     '''
     # Check if the tree is valid. If it is not, return 0.
     if root is None: return 0
-    else: 
-        root.visited_from = "root"
-        #root.traversed = True 
+    else: root.visited_from = "root"
 
     # Declare a queue and enqueue the starting node.
     q = queue.Queue()         
@@ -43,17 +41,7 @@ def bredth_first(maze_data, root, goal):
         if cur.traversed is False:
             if cur.data is 'P': cur.traversed = True
             if cur.data is goal: 
-                while cur.data is not 'P':
-                    if cur.visited_from == "down":
-                        cur = cur.down
-                    elif cur.visited_from == "up":
-                        cur = cur.up
-                    elif cur.visited_from == "right":
-                        cur = cur.right
-                    elif cur.visited_from == "left":
-                        cur = cur.left 
-                    if cur.data is not '.' and cur.data is not 'P':
-                        maze_data[cur.x][cur.y] = '^'
+                retrace(cur, maze_data)
                 return 1
             else:                 
                 if cur.up is not None and cur.up.data is not '%': 
@@ -77,16 +65,59 @@ def bredth_first(maze_data, root, goal):
     # Return 0 if goal was not found
     return 0
 
-def depth_first():
+def depth_first(maze_data, root, goal):
     ''' depth_first
         Griffin A. Tucker
-        FINAL DATE
+        February 3 2018
         This function performs a depth-first search on
             a given graph.
         This function returns 0 for now.
+        Accepts:
+            maze_data : a 2d array of characters 
+            root  : a tree to perform the search on
+            goal  : a goal character value to search for
+        Returns:
+            1 : if the goal is found
+            0 : if the goal is not found
     '''
-    # Declare a stack
+    # Check if the tree is valid. If it is not, return 0.
+    if root is None: return 0
+    else: root.visited_from = "root"
+
+    # Declare a stack and push the root node
     s = stack.Stack()
+    s.push(root)
+
+    # While the stack is not empty, get the next node on the stack
+    # and check for the goal. If at the goal, copy the successful
+    # path to the array of mazedata and then return 1. Else,
+    # expand the node to the stack. 
+    while s.size() > 0:  
+        cur = s.pop()
+        if cur.traversed is False:
+            if cur.data is 'P': cur.traversed = True
+            if cur.data is goal: 
+                retrace(cur, maze_data)
+                return 1
+            else:                 
+                if cur.up is not None and cur.up.data is not '%': 
+                    s.push(cur.up)
+                    if cur.up.visited_from == "not":
+                        cur.up.visited_from = "down"
+                if cur.down is not None and cur.down.data is not '%': 
+                    s.push(cur.down)
+                    if cur.down.visited_from == "not":
+                        cur.down.visited_from = "up"
+                if cur.left is not None and cur.left.data is not '%': 
+                    s.push(cur.left)
+                    if cur.left.visited_from == "not":
+                        cur.left.visited_from = "right"
+                if cur.right is not None and cur.right.data is not '%': 
+                    s.push(cur.right)
+                    if cur.right.visited_from == "not":
+                        cur.right.visited_from = "left"
+            cur.traversed = True
+
     # Return result 
     return 0
 
@@ -109,3 +140,42 @@ def a_star():
         This function returns 0 for now.
     '''
     return 0
+
+def retrace(goal, maze_data):
+    ''' retrace
+        Griffin A. Tucker
+        February 3 2018
+        This function alters an array of given mazedata
+            so that it holds a return path from a goal
+            to a start
+        Accepts:
+            maze_data : a 2d array of characters 
+            goal  : a goal node to begin the retrace on
+        Returns:
+            1 : if we reach the start 
+            0 : if data is invalid
+    '''
+    # Check that the supplied data is valid
+    if goal is None or maze_data is None:
+        return 0
+
+    # Set the current node being visited as the provided
+    # goal node.
+    cur = goal
+
+    # Traverse a back path from the goal node to the start
+    # node. 
+    while cur.data is not 'P':
+        if cur.visited_from == "down":
+            cur = cur.down
+        elif cur.visited_from == "up":
+            cur = cur.up
+        elif cur.visited_from == "right":
+            cur = cur.right
+        elif cur.visited_from == "left":
+            cur = cur.left 
+        if cur.data is not '.' and cur.data is not 'P':
+            maze_data[cur.x][cur.y] = '^'
+
+    # Return successful
+    return 1
